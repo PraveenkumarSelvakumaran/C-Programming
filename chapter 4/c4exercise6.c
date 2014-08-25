0a39071b5593bc6eee6bf4c0e63c362ea8672e73
orcarajkumar <linuxraaja@gmail.com>
2014-08-25 23:33:24 +0530

    c4 exercise 6

---
+++ chapter 4/c4exercise6.c
@@ -0,0 +1,16 @@
+/* chapter 4 exercise 4 polynomial evaluation
+ * rajkumar kandasamy
+ */
+ 
+ #include<stdio.h>
+ 
+ int main()
+ {
+	float x = 2.55;
+	float y;
+
+	y = 3 * x * x * x - 5 * x * x + 6;
+	printf("for x = 2.55 y = %f", y);
+
+	return 0;
+}
