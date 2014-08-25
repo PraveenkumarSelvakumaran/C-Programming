16f930705075f4764203b1d660ddc64009348590
orcathayumanavan <thayu13@gmail.com>
2014-08-25 16:01:22 -0700

    commit 1

---
+++ c4example1.c
@@ -0,0 +1,23 @@
+/* Program Using Basic Data types
+ * rajkumar kandasamy, 8/25/2014
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int integerVar = 100;
+	float floatingVar = 331.79;
+	double doubleVar = 8.44e + 11;
+	char charVar = 'W';
+
+	printf ("integerVar = %i\n", integerVar);
+	printf ("floatingVar = %f\n", floatingVar);
+	printf ("doubleVar = %e\n", doubleVar);
+	printf ("doubleVar = %g\n", doubleVar);
+	printf ("charVar = %c\n", charVar);
+
+	return 0;
+}
+
+
